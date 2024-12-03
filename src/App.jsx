a86395efc4bd985c9b2f7e44dcf631975b4b0db2
GalloSimone <gallosimonecz@gmail.com>
2024-12-03 16:16:20 +0100

    add structure html

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,27 @@
+import { useState } from 'react'
 
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+<div className="container">
+  <div className="header">
+        <h1>IL MIO BLOG</h1>
+  </div>
+  <form>
+     <input 
+       type="text" 
+       className='form-control'
+       />
+   <button className='btn btn-primary m-5'>
+        aggiungi
+    </button>
+  </form>
+      †
+</div>
+    </>
+  )
+}
+
+export default App
